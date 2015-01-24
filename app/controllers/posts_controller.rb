c2157bace6ee2236ee7d3b62c75b3df4c2622ca8
Hong ChulJu <ang0123dev@gmail.com>
2015-01-24 16:56:43 +0900

    post function added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @new_post = Post.new
+    @posts = Post.latest
   end
 
   def create
+    @new_post = Post.new(post_params)
+
+    if @new_post.save
+      redirect_to posts_path
+    else
+      # @new_post = Post.new
+      @posts = Post.latest
+      render 'index'
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:link, :content)
+    end
 end
