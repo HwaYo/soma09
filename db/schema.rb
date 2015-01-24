3ace34dbc56ce59642c47fae8079836d7b57c925
DevSusu <os1742@gmail.com>
2015-01-24 15:14:47 +0900

    Completed Facebook SignUp and Login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124054302) do
+ActiveRecord::Schema.define(version: 20150124061013) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "link"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20150124054302) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
