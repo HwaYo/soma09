640b0e2897cc2f145be88f74f1bbb34c92e9d1ab
김민혁 <minhyeok92@nhnnext.org>
2015-01-30 16:41:18 -0500

    can edit posts

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,4 +2,27 @@ $(document).on('ready page:load', function () {
   $(".btn-post-close").on('click', function (e) {
     $(this).addClass('disabled');
   });
+
+  $(".btn-post-edit").on("click", function(e){
+    var link = $(e.target);
+    var post_id = link.data('post');
+    
+    $.ajax({
+      url: "posts/" + post_id + "/edit"
+    }).success(function(html){
+        $('.modal-body').html(html);
+    });
+  });
+
+  $("#editModal").on("shown.bs.modal", function(e) {
+    $('.modal-footer input[type="submit"]').on("click", function(e){
+      if($('.modal-body textarea[name$="[content]"]').val() != "" && $('.modal-body input[name$="[link]"]').val() != "") {
+        $('#editModal form').submit();
+      }
+      else {
+        e.preventDefault();
+        alert("빈 칸을 채워주세요.");
+      }
+    });
+  });
 });
