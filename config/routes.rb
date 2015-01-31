673799fbb46d497802c1d675ad98b658df315de4
김민혁 <minhyeok92@nhnnext.org>
2015-01-31 10:59:13 -0500

    2. specify using routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  resources :posts do
+  resources :posts, only: [:index, :create, :destroy] do
     member do
       get 'close'
       get 'open'
