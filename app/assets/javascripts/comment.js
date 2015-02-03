74804ec6c65b6f671664215c5bb4faa7e70f8242
DevSusu <os1742@gmail.com>
2015-02-03 18:36:50 -0500

    Users can edit comments.

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
   
-  $('.comment_input').on('keypress', function(e) {
+  $('.comment_input').on('keyup', function(e) {
     
     if ($(this).val().length != 0) {
       $(this).parent().find('.comment_submit').removeClass("disabled");
     } else if(e.keyCode == 13) {
+      console.log('Enter Pressed');
+      e.preventDefault();
       return false;
     } else {
       $(this).parent().find('.comment_submit').addClass("disabled");
     }
   });
 
-  $('#edit-comment').on('click', function(e) {
+  $('.btn-xs, .btn-orange').on('click', function(e) {
     // find the comment box and change it to a text-input form
+    $('.btn-xs, .btn-orange').addClass('disabled');
+
     commentBox = $(this).parent().prev();
     comment = commentBox.find('p');
 
-    commentId = $(document).find('#comment-id').attr('value');
+    commentId = $(this).parent().prev().find('#comment-id').attr('value');
     commentForm = $(this).closest('.panel-info').next().find('form');
     
     updateButton = commentForm.find('input').last();
     updateButton.addClass("btn-orange");
     updateButton.removeClass("btn-info");
     updateButton.attr('value',"수정");
+    updateButton.attr('type','button');
+    updateButton.attr('id','comment-update-btn');
 
     // Change Form attributes to Work as updates
     var action = commentForm.attr('action') +"/"+ commentId;
     commentForm.attr('action', action);
-    commentForm.attr('method','PUT');
-    commentForm.find('.comment_input').attr('value',comment.text());
+
+    if( commentForm.find('_method') )
+      commentForm.find('_method').remove();
+
+    var putInput = $('<input type="hidden" name="_method" value="patch"></input>');
+    commentForm.append(putInput);
+
+    commentForm.find('.comment_input').attr('value',comment.text()).attr('id','#comment-update-input');
 
     $(this).parent().find('.btn').first().remove();
-
-    /* use ajax to send a PATCH request
-utf8:✓
-authenticity_token:OODm+lY2Aml13xHVGcGpN//Sd8N6hMY6wXRBOZ7Qy+Tke2/DY3LyKV6s/QI8hVz4/as0ilplpyJS07XkLs3qDA==
-comment[post_id]:7
-comment[content]:afadsf
-commit:댓글
-    */
     
   });
 
